4300b3218dcb8cac93d9d481a66dd7357e0e2051
ptak-warsaw-expo-dev <68947073+ptak-warsaw-expo-dev@users.noreply.github.com>
2026-09-23 12:15:12 +0200

    Add files via upload

--- pwe-system.php
+++ pwe-system.php
@@ -3,7 +3,7 @@
  * Plugin Name: PWE System
  * Plugin URI: https://github.com/ptak-warsaw-expo-dev/pwe-system
  * Description: Central system for PWE tools and modules, including administration, integrations, data management and automation.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Requires PHP: 7.4
  * Author: PWE Web Developers
  * Co-author: Anton Melnychuk, Piotr Krupniewski, Jakub Choła
